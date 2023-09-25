5f890aed08ebf3100188a8b3ca2635d541ac0a1d
Jared Rickert <jaredrickert52@gmail.com>
2023-09-24 22:13:04 -0500

    style(common): add prettier autoinstaller

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	printWidth: 80,
+	endOfLine: 'auto',
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	overrides: [
+		{
+			files: ['*.yaml', '*.yml'],
+			tabWidth: 2,
+		},
+		{
+			files: '*.md',
+			tabWidth: 2,
+			proseWrap: true,
+			useTabs: false,
+		},
+	],
+};
